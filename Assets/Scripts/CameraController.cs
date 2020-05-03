35fa2ea1090d5f46c2ac56f16281d49bff6672de
Zygnius <twjia1998@gmail.com>
2020-05-03 15:37:11 -0400

    World transition animation and camera controller
    Worlds fade into each other when transitioning between them. The camera now smoothly follows the player.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private PlayerData playerData;
+    [SerializeField] private float smoothTime;
+#pragma warning restore 0649
+
+    private Vector2 velocity = Vector2.zero;
+
+    private void Update()
+    {
+        Vector2 movement = Vector2.SmoothDamp(transform.position, playerData.position, ref velocity, smoothTime);
+        transform.position = new Vector3(movement.x, movement.y, transform.position.z);
+    }
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -7,13 +7,58 @@ public class World : MonoBehaviour
 {
 #pragma warning disable 0649
     [SerializeField] private Tilemap[] tilemaps;
+    [SerializeField] private float fadeTime;
 #pragma warning restore 0649
+
+    private bool trackWorld;
+    private Coroutine routine;
+
+    private Color clearWhite = new Color(1, 1, 1, 0);
+
+    private void Awake()
+    {
+        trackWorld = tilemaps[0].gameObject.activeSelf;
+    }
 
     public void ToggleWorld()
     {
-        foreach (Tilemap item in tilemaps)
+        if(routine == null)
         {
-            item.gameObject.SetActive(!item.gameObject.activeSelf);
+            routine = StartCoroutine(FadeWorld());
         }
+    }
+
+    private IEnumerator FadeWorld()
+    {
+        if (!trackWorld)
+        {
+            foreach (Tilemap item in tilemaps)
+            {
+                item.gameObject.SetActive(!trackWorld);
+            }
+        }
+
+        float timer = 0;
+        while(timer < fadeTime)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+
+            foreach (Tilemap item in tilemaps)
+            {
+                if (trackWorld) item.color = Color.Lerp(Color.white, clearWhite, timer / fadeTime);
+                else item.color = Color.Lerp(clearWhite, Color.white, timer / fadeTime);
+            }
+        }
+
+        if (trackWorld)
+        {
+            foreach (Tilemap item in tilemaps)
+            {
+                item.gameObject.SetActive(!trackWorld);
+            }
+        }
+        trackWorld = !trackWorld;
+        routine = null;
     }
 }
